d1f2719f6deeb1edc92f248ae1531c4f5831fc65
Ryan Hou <rhou7873@gmail.com>
2025-10-14 22:47:46 -0400

    set up secrets logic & basic mongo connection

---
+++ src/bw_secrets.py
@@ -0,0 +1,34 @@
+"""Secrets management"""
+from bitwarden_sdk import BitwardenClient, DeviceType, client_settings_from_dict
+import os
+
+API_URL = os.getenv("BW_API_URL")
+IDENTITY_URL = os.getenv("BW_ID_URL")
+ACCESS_TOKEN = os.getenv("BW_ACCESS_TOKEN")
+
+if (API_URL is None or
+    IDENTITY_URL is None or
+        ACCESS_TOKEN is None):
+    raise Exception(
+        "Environment variables aren't set: "
+        f"API_URL={API_URL}, "
+        f"IDENTITY_URL={IDENTITY_URL}, "
+        f"ACCESS_TOKEN={ACCESS_TOKEN}")
+
+client = BitwardenClient(
+    client_settings_from_dict(
+        {
+            "apiUrl": API_URL,
+            "deviceType": DeviceType.SDK,
+            "identityUrl": IDENTITY_URL,
+            "userAgent": "Python",
+        }
+    )
+)
+
+client.access_token_login(ACCESS_TOKEN)
+
+################# GLOBAL CONSTANTS #################
+
+MONGO_CONN_STR = client.secrets().get(
+    "32d23546-9159-491a-88dd-b3770028fbef").data.value
--- src/res_req.py
+++ src/res_req.py
@@ -1,1 +1,39 @@
-print("res_req new")
+import json
+from pymongo import MongoClient
+from pydantic import BaseModel, EmailStr, model_validator
+from bw_secrets import MONGO_CONN_STR
+
+class Participant(BaseModel):
+    first_name: str
+    last_name: str
+    full_name: str | None = None
+    primary_email: EmailStr
+    all_emails: list[EmailStr] | None = None
+
+    @model_validator(mode="after")
+    def set_full_name(self):
+        if not self.full_name:
+            self.full_name = f"{self.first_name} {self.last_name}"
+        return self
+
+    @model_validator(mode="after")
+    def set_all_emails(self):
+        if not (self.all_emails and self.primary_email in self.all_emails):
+            self.all_emails = [self.primary_email]
+        return self
+
+    def __str__(self):
+        return self.full_name
+
+
+MONGO_CLIENT: MongoClient = MongoClient(MONGO_CONN_STR)
+
+db = MONGO_CLIENT.get_database("res-req")
+participants_collection = db.get_collection("participants")
+
+participants: list[Participant] = []
+for doc in participants_collection.find():
+    participants.append(Participant.model_validate(doc))
+
+for participant in participants:
+    print(participant)
